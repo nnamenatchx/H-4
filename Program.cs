e71f770afadc3d58eb99b4cc87625b35fc83d462
nnamenatchx <122606565+nnamenatchx@users.noreply.github.com>
2023-05-20 02:29:02 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿
+class Program {
+    static void Main(string[] arg)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+        
+        int[] population = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+        int maxCustomers = 0;
+
+        for (int i = 0; i <= N - K; i++)
+        {
+            int customers = 0;
+
+            for (int j = i; j < i + K; j++)
+            {
+                customers += population[j];
+            }
+
+            if (customers > maxCustomers)
+            {
+                maxCustomers = customers;
+            }
+        }
+        Console.WriteLine(maxCustomers);
+    }
+}
